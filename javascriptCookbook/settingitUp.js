2c7a9dac9b34fd8d0abdf4c0d98a501c2089315c
dagimalemux <dagimalemux@gmail.com>
2023-09-21 21:12:29 +0300

    other js checks

---
+++ javascriptCookbook/settingitUp.js
@@ -0,0 +1,42 @@
+// There are 8 data types in the worlds most popular language
+
+/**
+ * number
+ * boolean
+ * bigInt(for large integers)
+ * string
+ * symbol(for unique identifiers)
+ * object(the root of every oter JS type)
+ * undefined(a variable that hasn't been assigned a value)
+ * null(a missing object)
+ */
+
+// Strings and Regular Expressions
+
+//----------------------------------------------------------------------------------
+// check if a variable is a string
+
+if(typeof unknownVariable == 'string'){
+    //unknownVariable is a string
+}
+
+// check non empty string
+
+if(typeof unknownVariable === 'string' && unknownVariable.length > 0){
+    // unknownVariable is a nonempty and string
+}
+
+// check or remove the extra space
+
+if(typeof unknownVariable === 'string' && unknownVariable.trim().length > 0){
+    // Now the number is string, notempty and trimmed
+}
+
+
+// other checks
+
+
+
+
+
+
