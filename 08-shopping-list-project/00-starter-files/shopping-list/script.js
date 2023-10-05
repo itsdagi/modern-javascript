8c6347794ed7986e665ab27a069b0c0f97ddc8e1
Dagim Alemu <143899556+dagialemu@users.noreply.github.com>
2023-10-05 15:11:09 +0300

    commit

--- 08-shopping-list-project/00-starter-files/shopping-list/script.js
+++ 08-shopping-list-project/00-starter-files/shopping-list/script.js
@@ -3,7 +3,7 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 
 function addItem(e) {
-    e.preventDefault();
+    e.preventDefault(); //e.preventDefault() is commonly used in web development to create more interactive and dynamic web applications where you want to control the behavior of elements in response to user interactions.
 }
 // Event Listners
 
