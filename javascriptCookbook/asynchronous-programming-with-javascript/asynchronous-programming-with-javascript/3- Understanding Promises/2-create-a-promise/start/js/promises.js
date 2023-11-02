f3d800b24ee0116d6ed750179852deb276833b06
Dagim Alemu <143899556+dagialemu@users.noreply.github.com>
2023-11-02 20:12:54 +0300

    c

--- javascriptCookbook/asynchronous-programming-with-javascript/asynchronous-programming-with-javascript/3- Understanding Promises/2-create-a-promise/start/js/promises.js
+++ javascriptCookbook/asynchronous-programming-with-javascript/asynchronous-programming-with-javascript/3- Understanding Promises/2-create-a-promise/start/js/promises.js
@@ -27,35 +27,32 @@ function getProfiles(json) {
   const Profiles = json.people.map( person => {
     return getJSON(wikiUrl + person.name);      
   }); 
-  return Profiles;
+  return Promise.all(Profiles);
 }
 
 // Generate the markup for each profile
 function generateHTML(data) {
-  const section = document.createElement('section');
+  data.map(person =>{
+    const section = document.createElement('section');
   peopleList.appendChild(section);
   // Check if request returns a 'standard' page from Wiki
   if (data.type === 'standard') {
     section.innerHTML = `
-      <img src=${data.thumbnail.source}>
-      <h2>${data.title}</h2>
-      <p>${data.description}</p>
-      <p>${data.extract}</p>
+      <img src=${person.thumbnail.source}>
+      <h2>${person.title}</h2>
+      <p>${person.description}</p>
+      <p>${person.extract}</p>
     `;
-  } else {
-    section.innerHTML = `
-      <img src="img/profile.jpg" alt="ocean clouds seen from space">
-      <h2>${data.title}</h2>
-      <p>Results unavailable for ${data.title}</p>
-      ${data.extract_html}
-    `;
-  }
+}})
 }
-
+  
 btn.addEventListener('click', (event) => {
+
+  event.target.textContent = "Loading ....."
   getJSON(astrosUrl)
   .then(getProfiles)
-  .then(data => console.log(data))
+  .then(generateHTML)
   .catch(err => console.log(err))
-  event.target.remove()
-});
+  .finally(()=> event.target.remove())
+  
+})
