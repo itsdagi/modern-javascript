3102cbf619de80769477dd3a166ea21ec2dd6634
Dagim Alemu <dagimalemux@gmail.com>
2023-11-09 17:31:45 +0300

    list items

--- 08-shopping-list-project/00-starter-files/shopping-list/script.js
+++ 08-shopping-list-project/00-starter-files/shopping-list/script.js
@@ -5,11 +5,34 @@ const itemInput = document.getElementById('item-input');
 function addItem(e){
   e.preventDefault();
   // validate input
-  if(itemInput.value === ''){
+  newItem = itemInput.value
+  if(newItem === ''){
     alert('Please add an item')
     return;
   }
-console.log('sucess');
+ // create list item 
+ const li = document.createElement('li');
+ li.appendChild(document.createTextNode(newItem))
+
+  const button = createButton('remove-item btn-link text-red')
+  li.appendChild(button)
+  itemList.appendChild(li);
+  itemInput.value = '';
+}
+
+function createButton(classes)
+{
+  const button = document.createElement('button')
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark')
+  button.appendChild(icon)
+  return button;
+}
+
+function createIcon(classes){
+  const icon = document.createElement('i');
+icon.className = classes;
+return icon;
 }
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
